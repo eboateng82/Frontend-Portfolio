4966c6219bc3ffc1ed04fde573e0eb493e57025e
enoch boateng <enochboateng82@gmail.com>
2020-09-28 08:37:24 -0700

    merging all assignments into one repository

---
+++ quizapp/questions.js
@@ -0,0 +1,72 @@
+var questions = [{
+ "question":"In what city was Jesus born?",
+ "option1": "Jerusalem",
+ "option2": "Bethlehem",
+ "option3": "Bethsaida",
+ "option4": "Golgota",
+ "answer": "2"
+},{
+ "question":"How many books are in the New Testament?",
+ "option1": "20",
+ "option2": "33",
+ "option3": "27",
+ "option4": "40",
+ "answer": "3"
+},{
+ "question":"What type of insect did John the Baptist eat in the desert?",
+ "option1": "Butterflies",
+ "option2": "Mosquitoes",
+ "option3": "Beetles",
+ "option4": "Locusts",
+ "answer": "4"
+},{
+ "question":"Who were the first apostles called to follow Jesus?",
+ "option1": "Peter and Andrew",
+ "option2": "Peter and James",
+ "option3": "Peter and Philip",
+ "option4": "Peter and Judas",
+ "answer": "1"
+},{
+ "question":"How many people did Jesus feed with five loaves of bread and two fish?",
+ "option1": "about 3000 men",
+ "option2": "about 4000 men",
+ "option3": "about 5000 men",
+ "option4": "about 6000 men",
+ "answer": "3"
+},{
+ "question":"After Jesus was arrested, which apostle disowned him three times?",
+ "option1": "James",
+ "option2": "Philip",
+ "option3": "Judas",
+ "option4": "Peter",
+ "answer": "4"
+},{
+ "question":"Who recognized Jesus as the Messiah when he was presented at the Temple as a baby?",
+ "option1": "Simon",
+ "option2": "Peter",
+ "option3": "Simeon",
+ "option4": "John the Baptist",
+ "answer": "3"
+},{
+ "question":"Who asked Pilate for Jesus' body after the crucifixion?",
+ "option1": "Joseph of Arimathea",
+ "option2": "Mary Magdalene",
+ "option3": "Peter",
+ "option4": "Philip",
+ "answer": "1"
+},{
+ "question":"Paul was shipwrecked on what island?",
+ "option1": "Patmos",
+ "option2": "Malta",
+ "option3": "Seychelles",
+ "option4": "Madagasca",
+ "answer": "2"
+},{
+ "question":"What is the shortest book in the New Testament?",
+ "option1": "Jude",
+ "option2": "1 John",
+ "option3": "2 John",
+ "option4": "3 John",
+ "answer": "3"
+
+}]
